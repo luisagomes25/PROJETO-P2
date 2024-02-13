660baf5b68a52cf2bc270c52b43ded7c5c6faec4
luis <luisfelipepippo@gmail.com>
2024-02-13 15:16:57 -0300

    teste 3

--- Miniprojeto.java
+++ Miniprojeto.java
@@ -2,7 +2,22 @@ import java.util.Scanner;
 
 public class Miniprojeto{
     public static void main(String[] args) {
-        
+            String capitulo1 = "Um belo dia luis resolve se aventurar em uma floresta e encontra um caminho.";
+
+            String escollha1capitulo1 = "Ele segue o caminho";
+            String escollha2capitulo1 = "Ele volta para casa";
+
+            String capitulo2 = "Ao seguir o caminho, ele encontra uma casa abandonada";
+
+            String escolha1capitulo2 = "Ele entra na casa e encontra um bau";
+            String escolha2capitulo2 = "Ele da uma volta na casa";
+
+            String capitulo3 = "Ao abrir esse bau ele encontra uma chave e logo avista uma porta nos fundos";
+
+            String capitulo4 = "Na lateral da casa ele encontra uma armadilha e acaba morrendo";
+
+            
+
         }
 
     }   
